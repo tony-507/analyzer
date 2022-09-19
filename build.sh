6a38dc49571c898096cb9040949857aba0ca1f22
Tony Chan <tony.chan@harmonicinc.com>
2022-09-19 22:51:57 +0800

    Add basic CI/CD build flow

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+curDir=$(pwd)
+echo "Current directory: $curDir"
+
+echo "Begin CI/CD workflow"
+rm -rf build
+mkdir -p build
+cd build
+
+build_opt=0 # 0: full build, 1: build without test
+
+if (($1)); then
+	build_opt=1
+fi
+
+echo "Building tsa\n"
+go build $curDir/cmd/tsa/main.go
+mv main tsa
+
+echo "Building unitTest\n"
+go build $curDir/cmd/unitTest/main.go
+mv main unitTest
+
+echo "Start running tests\n"
+./unitTest
