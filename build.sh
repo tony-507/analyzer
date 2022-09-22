a748f3e194648449060b316829460de48344d192
Tony Chan <tony.chan@harmonicinc.com>
2022-09-22 17:33:22 +0800

    Update build script for flexibility

--- build.sh
+++ build.sh
@@ -1,29 +1,84 @@
 #!/bin/bash
 
-curDir=`dirname -- "$(readlink -f -- $0)"`
-echo "Current directory: $curDir"
+showHelp() {
+	echo "Usage: sh build.sh <options> <flags>"
+}
 
-echo "Begin CI/CD workflow"
-rm -rf build
-mkdir -p build/resources
-cd build
+init() {
+	curDir=`dirname -- "$(readlink -f -- $0)"`
+	echo "Current directory: $curDir"
 
-build_opt=0 # 0: full build, 1: build without test
+	echo "Begin CI/CD workflow"
+	rm -rf build
+	mkdir -p build/resources
+	cd build
+}
 
-if (($1)); then
-	build_opt=1
+build_app() {
+	echo "Building tsa\n"
+	# Executable
+	go build $curDir/cmd/tsa/main.go
+	mv main tsa
+	# Resources for app
+	cp $curDir/src/resources/app.json $curDir/build/resources/.
+}
+
+build_test() {
+	echo "Building unitTest\n"
+	go build $curDir/cmd/unitTest/main.go
+	mv main unitTest
+}
+
+run_test() {
+	echo "Start running tests\n"
+	./unitTest
+}
+
+runTest=true
+
+if [ "$#" -gt 0 ];then
+	case $1 in
+		"build")
+			case $2 in
+				"-x")
+					case $3 in
+						"test")
+							runTest=false
+							;;
+						"*")
+							showHelp
+							;;
+						esac
+					;;
+
+				"*")
+					showHelp
+					;;
+			esac
+
+			init
+			build_app
+			build_test
+
+			if $runTest;then
+				run_test
+			fi
+			;;
+
+		"test")
+			cd build
+			run_test
+			;;
+		
+		"help")
+			showHelp
+			;;
+		"*")
+			showHelp
+			Exit 1
+			;;
+	esac
+	echo "Done"
+else
+	showHelp
 fi
-
-echo "Building tsa\n"
-# Executable
-go build $curDir/cmd/tsa/main.go
-mv main tsa
-# Resources for app
-cp $curDir/src/resources/app.json $curDir/build/resources/.
-
-echo "Building unitTest\n"
-go build $curDir/cmd/unitTest/main.go
-mv main unitTest
-
-echo "Start running tests\n"
-./unitTest
