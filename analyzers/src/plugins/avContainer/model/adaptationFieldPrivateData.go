86592c7990e33fc12a1c372f4898dfde856c6a93
Tony Chan <tony.chan@harmonicinc.com>
2024-01-24 12:00:57 +0800

    Support EBP info

---
+++ analyzers/src/plugins/avContainer/model/adaptationFieldPrivateData.go
@@ -0,0 +1,96 @@
+package model
+
+import (
+	"encoding/json"
+	"errors"
+
+	"github.com/tony-507/analyzers/src/plugins/common/io"
+)
+
+type ebpInfo struct {
+	AcquisitionTime  uint64
+	ExtPartition     int
+	GroupingIds      []int
+	SAPType          int
+}
+
+func parseEbpInfo(r *io.BsReader) ([]byte, error) {
+	ebp := ebpInfo{}
+
+	if r.ReadChar(4) != "EBP0" {
+		return []byte{}, errors.New("Format identifier is not EBP0")
+	}
+
+	r.ReadBits(1) // Fragment
+	r.ReadBits(1) // Segment
+	bSAP := r.ReadBits(1) != 0
+	bGrouping := r.ReadBits(1) != 0
+	bTime := r.ReadBits(1) != 0
+	r.ReadBits(1) // Conceal
+	r.ReadBits(1)
+	bExtension := r.ReadBits(1) != 0
+	extPartitionFlag := false
+
+	if bExtension {
+		extPartitionFlag = r.ReadBits(1) != 0
+		r.ReadBits(7)
+	}
+
+	if bSAP {
+		ebp.SAPType = r.ReadBits(3)
+		r.ReadBits(5)
+	}
+
+	if bGrouping {
+		hasNext := r.ReadBits(1) != 0
+		ebp.GroupingIds = []int{r.ReadBits(7)}
+		for hasNext {
+			hasNext = r.ReadBits(1) != 0
+			ebp.GroupingIds = append(ebp.GroupingIds, r.ReadBits(7))
+		}
+	}
+
+	if bTime {
+		ebp.AcquisitionTime = uint64(r.ReadBits(64))
+	}
+
+	if extPartitionFlag {
+		ebp.ExtPartition = r.ReadBits(8)
+	}
+
+	r.ReadBits(len(r.GetRemainedBuffer()) * 8)
+
+	return json.Marshal(ebp)
+}
+
+type privateData struct {
+	tag    int
+	length int
+	data   string // JSON string if known, otherwise hex string
+}
+
+func (pd *privateData) getData() string {
+	return pd.data
+}
+
+func parsePrivateData(r *io.BsReader) (privateData, error) {
+	pd := privateData{}
+
+	pd.tag = r.ReadBits(8)
+	pd.length = r.ReadBits(8)
+
+	var err error
+	var data []byte
+	switch pd.tag {
+	case 0xDF:
+		data, err = parseEbpInfo(r)
+	default:
+		pd.data = r.ReadChar(pd.length)
+	}
+
+	if len(data) != 0 {
+		pd.data = string(data)
+	}
+
+	return pd, err
+}
